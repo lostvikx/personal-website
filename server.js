ecf92a7dc84f6af6f5d079a957cb30ac2247ad37
Vikram S. Negi <viknegi0@gmail.com>
2022-02-04 11:41:27 +0530

    setup server

--- server.js
+++ server.js
@@ -3,4 +3,17 @@
 const express = require("express");
 const app = express();
 
-express.static("/", __dirname + "/public");
+app.use("/", express.static(__dirname + "/public"));
+
+// 404 Page
+app.get("*", (req, res) => {
+
+  res.status(404).send("nani?")
+
+})
+
+const HOST = "localhost"
+const PORT = process.env.PORT | 3000;
+
+app.listen(PORT, HOST, () => console.log(`listening on http://${HOST}:${PORT}`));
+
