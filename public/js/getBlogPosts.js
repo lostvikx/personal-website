f336fa11d66beb55a618c4e7aa579e647c3b3736
Vikram S. Negi <viknegi0@gmail.com>
2022-02-16 19:45:09 +0530

    refactored and ideas

--- public/js/getBlogPosts.js
+++ public/js/getBlogPosts.js
@@ -9,4 +9,46 @@ const getAllBlogPosts = async () => {
 
 }
 
-export { getAllBlogPosts }
+const createPost = (data) => {
+
+  const post = document.createElement("div");
+  post.className = "post";
+  
+  const a = document.createElement("a");
+  a.href = data.pathToHTMLFile;
+
+  post.append(a);
+  
+  const postDiv = document.createElement("div");
+  postDiv.className = "post-div";
+  a.append(postDiv);
+
+  const info = document.createElement("div");
+  info.className = "post-info";
+
+  const h3 = document.createElement("h3");
+  h3.textContent = data.postTitle;
+  h3.className = "post-title";
+  const p = document.createElement("p");
+  p.textContent = data.postSubject;
+  p.className = "post-subject";
+  const time = document.createElement("p");
+  time.textContent = data.timeCreated;
+  time.className = "time-created";
+
+  info.append(h3, p, time);
+
+  const img = document.createElement("img");
+  img.src = data.thumbnail;
+  img.alt = "blog post thumbnail";
+
+  const postImg = document.createElement("div");
+  postImg.className = "post-img";
+  postImg.append(img);
+
+  postDiv.append(postImg, info);
+
+  return post;
+}
+
+export { getAllBlogPosts, createPost }
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { getAllBlogPosts } from "./getBlogPosts.js";
+import { getAllBlogPosts, createPost } from "./getBlogPosts.js";
 
 console.log(window.location);
 
@@ -33,52 +33,14 @@ try {
 
 const pathName = window.location.pathname;
 
-const createPost = (data) => {
-
-  const post = document.createElement("div");
-  post.className = "post";
-  
-  const a = document.createElement("a");
-  a.href = data.pathToHTMLFile;
-
-  post.append(a);
-  
-  const postDiv = document.createElement("div");
-  postDiv.className = "post-div";
-  a.append(postDiv);
-
-  const info = document.createElement("div");
-  info.className = "post-info";
-
-  const h3 = document.createElement("h3");
-  h3.textContent = data.postTitle;
-  h3.className = "post-title";
-  const p = document.createElement("p");
-  p.textContent = data.postSubject;
-  p.className = "post-subject";
-  const time = document.createElement("p");
-  time.textContent = data.timeCreated;
-  time.className = "time-created";
-
-  info.append(h3, p, time);
-
-  const img = document.createElement("img");
-  img.src = data.thumbnail;
-  img.alt = "blog post thumbnail";
-
-  const postImg = document.createElement("div");
-  postImg.className = "post-img";
-  postImg.append(img);
-
-  postDiv.append(postImg, info);
-
-  return post;
-}
-
 if (pathName === "/blog/" || pathName === "/blog/index.html" || pathName === "/blog/index") {
   
   // get all posts
   const allPosts = await getAllBlogPosts();
+
+  // TODO: sorting by latest
+  // TODO: create meta-attribute for article tags
+
   console.log(allPosts);
 
   const allPostsUl = document.getElementById("all-posts");
