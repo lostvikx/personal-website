a4a5685791b8563a0edd7451a5f939eb75d9bf9e
Vikram S. Negi <viknegi0@gmail.com>
2022-02-20 14:03:13 +0530

    few changes in server.js

--- server.js
+++ server.js
@@ -3,25 +3,15 @@
 const express = require("express");
 const app = express();
 
-// This might not be perfect!
+// This might not be so perfect!
 app.use("/", express.static(__dirname + "/public", {extensions:['html']}));
 
 const HOST = "localhost";
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, HOST, () => console.log(`listening on http://${HOST}:${PORT}`));
 
-// send md file
-// app.get("/about", (req, res) => {
-
-//   res.sendFile(__dirname + "/about.md", (err) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//   })
-
-// });
-
+// send json file data
 app.get("/blog/all-posts", (req, res) => {
   res.sendFile(__dirname + "/db/blog-info.json", (err) => {
     if (err) console.log(err);
