1aecf96c7b6e0090a660ab1fa5a8072fc06372c2
Vikram S. Negi <viknegi0@gmail.com>
2022-02-20 15:07:10 +0530

    optimized to calc reading time when on a blog post

--- public/js/main.js
+++ public/js/main.js
@@ -7,31 +7,36 @@ console.log(window.location);
 // pre code hightlights
 hljs.highlightAll();
 
-// Article reading time
-const calcReadingTime = () => {
-  const articleText = document.getElementById("article").innerText;
-  const wordsPerMinute = 180; // This is a good reading speed to understand the content.
-  const nWords = articleText.trim().split(/\s+/).length;
-  const time = Math.ceil(nWords / wordsPerMinute)
-
-  return `${time} ${time == 1 ? "minute" : "minutes"}`;
-}
-const updatePostInfo = () => {
-  const postInfo = document.getElementById("post-info");
-  const readingTime = document.createElement("p")
-  readingTime.className = "post-meta";
-  readingTime.textContent = `Reading Time: ${calcReadingTime()}`
-  postInfo.appendChild(readingTime);
-}
-
-try {
-  updatePostInfo();
-} catch (err) {
-  console.log("Reading time was not shown.");
-  // console.log(err);
-}
-
 const pathName = window.location.pathname;
+const isBlogPost = /^\/blog\/[\S]+/.test(pathName);
+
+if (isBlogPost) {
+  console.log("is a blog post!");
+  // Article reading time
+  const calcReadingTime = () => {
+    const articleText = document.getElementById("article").innerText;
+    const wordsPerMinute = 180; // This is a good reading speed to understand the content.
+    const nWords = articleText.trim().replace(/[^\w\s\d]/g, "").split(/\s+/).length;
+    const time = Math.ceil(nWords / wordsPerMinute)
+
+    return `${time} ${time == 1 ? "minute" : "minutes"}`;
+  }
+  
+  const updatePostInfo = () => {
+    const postInfo = document.getElementById("post-info");
+    const readingTime = document.createElement("p");
+    readingTime.className = "post-meta";
+    readingTime.textContent = `Reading Time: ${calcReadingTime()}`;
+    postInfo.appendChild(readingTime);
+  }
+
+  try {
+    updatePostInfo();
+  } catch (err) {
+    console.log("Reading time was not shown.");
+    // console.log(err);
+  }
+}
 
 if (pathName === "/blog/" || pathName === "/blog/index.html" || pathName === "/blog/index") {
   
